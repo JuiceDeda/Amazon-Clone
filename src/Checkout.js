7de95df1b9921232004817c7ec7af853afd5bc29
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-18 21:44:26 +0000

    2 files modified

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,17 @@ import "./Checkout.css";
 function Checkout () {
   return(
   <div className="checkout">
+    <div className="checkout__left">
 
+  
+      <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/MaloneLP/9XYZ_header.jpg"
+          alt="" />
+          
+      
+        
+      </div>
     </div>
 
   );
