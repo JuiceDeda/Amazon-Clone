ccb0fca4e4fe1fcb7d8f7d6b12b35ac5fce16ce2
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-18 19:31:00 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="app">
+      <Switch>
+        <Route path="/checkout">
+        <Header/>
+        <h1>Good Luck</h1>
+        </Route>
+     <Route path="/">
+      <Header/>
+      <Home/>
+      </Route>
+        </Switch>
     
-    <Header/>
-    <Home/>
+   
       </div>
-
+</Router>
   );
 }
 export default App;
