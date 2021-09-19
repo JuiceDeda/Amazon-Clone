c1023136692efadcc41bc82a0fe9d25cdd68886c
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-19 12:07:58 +0000

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
-
+import { Link } from "react-router-dom";
 function Header() {  
   return (
     <div className="header">
-      <Link to='/'>
-      <img className="header__logo"
+         <Link to="/">
+        <img
+          className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
       </Link>
+
+     
+     
 
          
       
@@ -41,7 +45,7 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        <Link to='/checkout'>
+        <Link to="/checkout">
         <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
@@ -49,7 +53,7 @@ function Header() {
             </span>
           </div>
         
-        </Link>
+       </Link>
          
       </div>
     </div>
