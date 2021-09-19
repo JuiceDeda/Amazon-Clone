f96edf77ade14b5feb42f650775e9dfcfe8bdf89
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-19 10:10:23 +0000

    2 files modified

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,7 +10,7 @@ function Checkout () {
   
       <img
           className="checkout__ad"
-          src="https://images-na.ssl-images-amazon.com/images/G/01/CurrencyConverter/ACCB-Banner-Desktop-EN.png"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/img17/prime/other/1053033_us_prime_dex_vxd-1060_DEX_LP_Banner_DT_v1.png"
           alt="" />
           
       
