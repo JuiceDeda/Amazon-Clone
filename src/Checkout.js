8eb8bb734aff360c03408e04f6b9d64a0f0d385b
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-19 10:41:50 +0000

    3 files modified

--- src/Checkout.js
+++ src/Checkout.js
@@ -13,7 +13,11 @@ function Checkout () {
           src="https://images-na.ssl-images-amazon.com/images/G/01/img17/prime/other/1053033_us_prime_dex_vxd-1060_DEX_LP_Banner_DT_v1.png"
           alt="" />
           
-      
+      <div>
+        <h2 className="checkout__title">
+          Your Shopping Basket
+        </h2>
+      </div>
         
       </div>
     </div>
