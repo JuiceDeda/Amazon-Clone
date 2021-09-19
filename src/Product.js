84c2e832c641a2e87864d82a42a8770287342681
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-19 16:45:50 +0000

    4 files modified, 3 added

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
+import { useStateValue } from "./StateProvider";
 function Product({ id, title, image, price, rating }) {
- 
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
   <div className="product">
     <div className="product__info">
@@ -22,7 +38,7 @@ function Product({ id, title, image, price, rating }) {
        
     </div>
     <img src={image} alt=""/>
-    <button>Add To Basket</button>
+    <button onClick={addToBasket}  >Add To Basket</button>
 </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
 import App from "./App";
+import * as serviceWorker from "./serviceWorker.js";
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
