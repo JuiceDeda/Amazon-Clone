67dc0195773e68cfee07b719741ef87376e1b0da
JuiceDeda <dedesidivall7in2@gmail.com>
2021-09-21 18:28:37 +0000

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Header from "./Header.js";
 import Home from "./Home.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Checkout from "./Checkout.js";
-
+import Login from "./Login.js";
 function App() {
   return (
     <Router>
     <div className="app">
-    <Header/>
+   
       <Switch>
+        <Route path="/login">
+          <Login/>
+          </Route>
         <Route path="/checkout">
-       
+        <Header/>
         <Checkout/>
        
         </Route>
      <Route path="/">
-      
+     <Header/>
       <Home/>
       </Route>
         </Switch>
---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Login.css'
+import { Link} from "react-router-dom";
+function Login() {
+  return (
+    <div className='login'>
+
+      <Link to='/'>
+   <img 
+   className="login__logo" src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'   />
+</Link>
+<div className="login__container">
+<h1>Sign-in</h1>
+<form>
+
+  <h5>E-mail</h5>
+  <input type='text'/>
+<h5> Password</h5>
+<input type='password'/>
+<button>Sign In</button>
+
+</form>
+
+
+</div>
+</div>
+    );
+}
+
+export default Login;
+
+
+
