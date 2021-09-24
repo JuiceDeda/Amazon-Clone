585cce7ed1a25027531dec79d784ccb69d02ef7f
JuiceDeda <86897571+JuiceDeda@users.noreply.github.com>
2021-09-24 12:07:50 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,53 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
-import Header from './components/Header';
-import Home from './components/Home';
-import Checkout from './components/Checkout'
-import Login from './components/Login'
-import { useStateValue } from './context/StateProvider'
-import { auth } from './firebase/firebase'
-
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
+import Login from "./components/Login";
+import { useStateValue } from "./context/StateProvider";
+import { auth } from "./firebase/firebase";
 
 function App() {
-
-  const [{}, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
   useEffect(() => {
-    // will only run once when the app component loads...
-
-    auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
-
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
-        // the user just logged in / the user was logged in
-
         dispatch({
           type: "SET_USER",
-          user: authUser,
+          user: authUser
         });
       } else {
-        // the user is logged out
         dispatch({
           type: "SET_USER",
-          user: null,
+          user: null
         });
       }
     });
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
+  console.log("user is -> ", user);
   return (
     <Router>
-    <div className="app">
-   
-      <Switch>
-        <Route path="/login">
-          <Login/>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
           </Route>
-        <Route path="/checkout">
-        <Header/>
-        <Checkout/>
-       
-        </Route>
-     <Route path="/">
-     <Header/>
-      <Home/>
-      </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
         </Switch>
-    
-   
       </div>
-</Router>
+    </Router>
   );
 }
 export default App;
-
-
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import { useStateValue } from '../context/StateProvider'
-import './Checkout.css'
-import CheckoutProduct from './CheckoutProduct'
-import Subtotal from './Subtotal'
-function Checkout () {
-  
-  const [{ basket}, dispatch] = useStateValue();
+import React from "react";
+import { useStateValue } from "../context/StateProvider";
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
+function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
 
-  return(
-  <div className="checkout">
-    <div className="checkout__left">
-
-  
-      <img
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/01/img17/prime/other/1053033_us_prime_dex_vxd-1060_DEX_LP_Banner_DT_v1.png"
-          alt="" />
-          
-      <div>
-        <h2 className="checkout__title">
-          Your Shopping Basket
-        </h2>
-        {basket.map(item => (
+          alt=""
+        />
+
+        <div>
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+          {basket.map((item) => (
             <CheckoutProduct
               id={item.id}
               title={item.title}
@@ -30,15 +27,12 @@ function Checkout () {
               rating={item.rating}
             />
           ))}
-      </div>
-        
+        </div>
       </div>
       <div className="checkout__rigt">
-        <Subtotal/>
-      
+        <Subtotal />
       </div>
     </div>
-
   );
 }
 
